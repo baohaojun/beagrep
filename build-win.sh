d8a717d373b567001fdf67bffbd3621b1e66c4c4
Bao Haojun <hjbao@marvell.com>
2011-12-22 15:45:21 +0800

    mf

--- build-win.sh
+++ build-win.sh
@@ -1,1 +1,14 @@
-CC="gcc -mno-cygwin -g" CXX="g++ -mno-cygwin -g" CFLAGS='-I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/atk-1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/cairo -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/FONTCO~1 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/FREETY~1 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/gail-1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/GDK-PI~1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/glib-2.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/gtk-2.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/LIBGLA~1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/libpng14 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/libxml2 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/mono-2.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/pango-1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include -I/cygdrive/c/PROGRA~1/MONO-2~1.6/lib/glib-2.0/include -I/usr/include' PKG_CONFIG_PATH=/cygdrive/c/PROGRA~1/MONO-2~1.6/lib/PKGCON~1 ./autogen.sh 
+#!/bin/bash
+set -ex
+export NOCONFIGURE=true
+export CC="gcc -mno-cygwin -g"
+export CXX="g++ -mno-cygwin -g"
+export CFLAGS='-I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/atk-1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/cairo -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/FONTCO~1 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/FREETY~1 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/gail-1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/GDK-PI~1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/glib-2.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/gtk-2.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/LIBGLA~1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/libpng14 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/libxml2 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/mono-2.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/pango-1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include -I/cygdrive/c/PROGRA~1/MONO-2~1.6/lib/glib-2.0/include -I/usr/include' 
+export PKG_CONFIG_PATH=/cygdrive/c/PROGRA~1/MONO-2~1.6/lib/PKGCON~1
+
+cd $(dirname $0)
+./autogen.sh
+./configure --prefix=$(wlp /usr/local)
+make -j8 -k
+make install
+cp /usr/local/lib/bin/cygbeagleglue-0.dll ~/bin/windows/ext/libbeagleglue.dll
