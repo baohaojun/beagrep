7b012939654c10b4ad49a5858c5bcbbb56f98cb5
Bao Haojun <hjbao@marvell.com>
2011-12-23 09:07:31 +0800

    mf

--- build-win.sh
+++ build-win.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 export NOCONFIGURE=true
-export CC="gcc -mno-cygwin -g"
-export CXX="g++ -mno-cygwin -g"
+export CC="i686-pc-mingw32-gcc -mno-cygwin -g"
+export CXX="i686-pc-mingw32-g++ -mno-cygwin -g"
 export CFLAGS='-I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/atk-1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/cairo -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/FONTCO~1 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/FREETY~1 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/gail-1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/GDK-PI~1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/glib-2.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/gtk-2.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/LIBGLA~1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/libpng14 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/libxml2 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/mono-2.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include/pango-1.0 -I/cygdrive/c/PROGRA~1/MONO-2~1.6/include -I/cygdrive/c/PROGRA~1/MONO-2~1.6/lib/glib-2.0/include -I/usr/include' 
 export PKG_CONFIG_PATH=/cygdrive/c/PROGRA~1/MONO-2~1.6/lib/PKGCON~1
 
