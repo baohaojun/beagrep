888367eba4069163434097ea5bc0fa6f4b772138
Bao Haojun <baohaojun@gmail.com>
2021-05-28 23:10:16 +0800

    auto
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using beagrep;
+
+namespace csharp
+{
+    class Program
+    {
+	static void Main(string[] args)
+	{
+	    Console.WriteLine("Hello World!");
+	    Program2.Hello3();
+	}
+    }
+}
